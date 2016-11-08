c0bb4288f988c9e3288e6343963fff626ed04f75
wagner.pinheiro <wagner@wagnerpinheiro.com.br>
2016-11-07 22:12:57 -0200

    adicionado a apresentação no menu

--- src/menu.php
+++ src/menu.php
@@ -5,7 +5,7 @@ $v = '1.4';
 
 
 $sections = array(
-	'Procedural'=>'/demo_mvc/procedural',
+  'Procedural'=>'/demo_mvc/procedural',
 	'OOP'=>'/demo_mvc/oop',
 	//'Funcional'=>'./demo_mvc/funcional',
 	'MVC'=>'/demo_mvc/mvc',	
@@ -28,12 +28,13 @@ if(!$sections){
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="/">Treinamento Symfony <?=$v?></a>
+      <a class="navbar-brand" href="/">Treinamento Symfony <?=$v?></a>      
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
+        <li><a target="_blank" href="/README.html">Apresentação</a></li>
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Demos <span class="caret"></span></a>
           <ul class="dropdown-menu">
